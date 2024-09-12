7c9f1f7c3f700365c39bfea8000bb1ff2f0399e3
b0vdurr <99080640@mydavinci.nl>
2024-09-12 22:40:20 +0200

    Create scooter.py

---
+++ Periode 1/Week 3/scooter.py
@@ -0,0 +1,9 @@
+band_prijs=57.79
+arbeid_per_band=15.00
+lampje_prijs=1.45
+lampje_vervangen=2.1
+
+versleten_band=2
+kapot_lampje=2
+
+print(round(((band_prijs +arbeid_per_band)*versleten_band + (lampje_prijs+lampje_vervangen)*kapot_lampje),2))
