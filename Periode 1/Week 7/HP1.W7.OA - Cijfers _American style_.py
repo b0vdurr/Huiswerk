f6ea8983cc8342250e5db19407fa2bcc809a4086
b0vdurr <99080640@mydavinci.nl>
2024-09-13 09:58:21 +0200

    Create HP1.W7.OA - Cijfers _American style_.py

---
+++ Periode 1/Week 7/HP1.W7.OA - Cijfers _American style_.py
@@ -0,0 +1,10 @@
+while True:
+    cijfer=float(input('Voer je cijfer in: '))
+    if cijfer > 10 or cijfer < 1:
+        print('Voer een correcte cijfer in')
+    else: break
+if cijfer >= 8.5:print('A')
+elif cijfer >= 7.5 and cijfer < 8.5:print('B')
+elif cijfer >= 6.5 and cijfer < 7.5:print('C')
+elif cijfer >= 5.5 and cijfer < 6.5:print('D')
+else: print('F')
