79b25ee63125b33b56f235262655ccf81f53c592
b0vdurr <99080640@mydavinci.nl>
2024-09-23 23:54:09 +0200

    Update HP1.W7.OB - Een nieuwe smartphone voor ....py

--- Periode 1/Week 7/HP1.W7.OB - Een nieuwe smartphone voor ....py
+++ Periode 1/Week 7/HP1.W7.OB - Een nieuwe smartphone voor ....py
@@ -1,8 +1,13 @@
 telefoons={'IPhone':0,'Samsung':0}
 telefoons['IPhone']=int(input('Hoeveel kost de IPhone?'))
 telefoons['Samsung']=int((input('Hoeveel kost de Samsung?')))
-print(f'De {max(telefoons)} is het duurst, de telefoon kost: {telefoons.get(max(telefoons))} Euro')
-print(f'De {min(telefoons)} is het goedkoopst, de telefoon kost: {telefoons.get(min(telefoons))} Euro')
-if telefoons.get('IPhone') - telefoons.get('Samsung') >= 50: advies = 'Samsung'
-else:advies = 'IPhone'
-print(f'Het advies is dus de {advies} te kopen')
+difference = telefoons['IPhone'] - telefoons['Samsung']
+
+print(difference)
+if difference < 0:
+    print(f'Het advies is dus de IPhone te kopen. Deze is namelijk {abs(difference)} euro goedkooper dan de Samsung')
+elif difference <= 50:
+    print(f'Het advies is dus de IPhone te kopen. Deze is namelijk {difference} euro duurder dan de Samsung')
+elif difference == 0:
+    print('Het advies is dus de IPhone te kopen. De prijs is hetzelfde')
+else: print(f'Het advies is dus de Samsung te kopen. Deze is namelijk {difference} euro goedkooper dan de Iphone')
