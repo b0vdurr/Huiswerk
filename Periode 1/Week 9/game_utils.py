0937282460b561e572116724b65732ba71d17786
b0vdurr <99080640@mydavinci.nl>
2024-10-12 18:03:06 +0200

    game

--- Periode 1/Week 9/game_utils.py
+++ Periode 1/Week 9/game_utils.py
@@ -1,25 +1,19 @@
 from time import sleep
-from pynput import keyboard
 from abc import ABC
 from termcolor import colored
 import random
 stop_printing = False
 
-def on_press(key):
-    global stop_printing
-    if key == keyboard.Key.enter:
-        stop_printing = True
 
 def delay_print(message: str):
-    global stop_printing
+    print('')
     for char in message:
         if stop_printing:
             print(message)
             break
         print(char, end='', flush=True)
-        sleep(0.04)
+        sleep(0.015)
     print('')
-    stop_printing = False
 
 def safe_input(message: str, possible_choices: list) -> int:
     while True:
@@ -31,179 +25,128 @@ def safe_input(message: str, possible_choices: list) -> int:
         except ValueError:
             delay_print('Enter an integer!')
 
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
-
 class Entity(ABC):
-    def __init__(self, name: str, strength: int, agility: int, intelligence: int, charisma: int,hp:int,armor:int):
+    def __init__(self, name: str, strength: int, agility: int, intelligence: int, charisma: int,hp:int):
         self.name = name
         self.strength = strength
         self.agility = agility
         self.intelligence = intelligence
         self.charisma = charisma
         self.hp=hp
-        self.armor=armor
+    def display_stats(self):
+        print('')
+        print(colored(f'\t{self.name}',attrs=['bold']))
+        print(colored(f'HP: {self.hp}',color='light_red'))
+        print(colored(f'ATTRIBUTES: ',attrs=['bold']))
+        print(colored(f'  Strength: {self.strength}/10','red'))
+        print(colored(f'  Agility: {self.agility}/10','green'))
+        print(colored(f'  Intelligence: {self.intelligence}/10','cyan'))
+        print(colored(f'  Charisma: {self.charisma}/10','light_yellow'))
+
 class AvailableCharacter(Entity):
-    def __init__(self, name: str, strength: int, agility: int, intelligence: int, charisma: int, hp:int, armor:int, nickname: str, age: int, bio: str, features: str):
-        super().__init__(name, strength, agility, intelligence, charisma)
+    def __init__(self, name: str, strength: int, agility: int, intelligence: int, charisma: int, hp:int, nickname: str, age: int, bio: str, features: str):
+        super().__init__(name, strength, agility, intelligence, charisma,hp)
         self.nickname = nickname
         self.age = age
         self.bio = bio
         self.features = features
-
-    def display_attributes(self):
-        print(colored(f'\tStrength: {self.strength}/10','red'))
-        print(colored(f'\tAgility: {self.agility}/10','green'))
-        print(colored(f'\tIntelligence: {self.intelligence}/10','cyan'))
-        print(colored(f'\tCharisma: {self.charisma}/10','light_yellow'))
     def display_bio(self):
-        print(colored(f'{self.name} - {self.nickname}', attrs=['bold']))
-        print(f'AGE: {self.age} years')
-        print('ATTRIBUTES')
-        self.display_attributes()
+        self.display_stats()
         print(colored('BACKGROUND:', attrs=['bold']))
         print(self.bio)
         print(colored('FEATURES:', attrs=['bold']))
         print(self.features)
 
 
-class Player(AvailableCharacter):
-    def __init__(self, name:str, strength:int, agility:int, intelligence:int, charisma:int, nickname:str, age:int, bio:str, features:str, hp=100, armor=0, items=None):
-        super().__init__(name, strength, agility, intelligence, charisma, nickname, age, bio, features)
-        self.hp = hp
-        self.armor = armor
-        self.items = items or []
-
-    def display_stats(self):
-        print(colored(f'HP: {self.hp}', 'light_red'))
-        print(colored(f'ARMOR: {self.armor}', 'grey'))
-        print('ITEMS:', ', '.join(map(str, self.items)) if self.items else 'No items')
-        print('')
-        print('ATTRIBUTES: ')
-        self.display_attributes()
+class Player(Entity):
+    def __init__(self, name:str, strength:int, agility:int, intelligence:int, charisma:int, hp=100):
+        super().__init__(name, strength, agility, intelligence, charisma,hp)
+    def is_dead(self):
+        if self.hp<0:
+            quit(colored('YOU ARE DEAD','red'))
+    
 class Enemy(Entity):
-    def __init__(self, name: str, strength: int, agility: int, intelligence: int, charisma: int, negotiable:bool=True):
-        super().__init__(name, strength, agility, intelligence, charisma)
+    def __init__(self, name: str, strength: int, agility: int, intelligence: int, charisma: int, hp:int=100,negotiable:bool=True):
+        super().__init__(name, strength, agility, intelligence, charisma,hp)
         self.negotiable=negotiable
 
+import random
+from time import sleep
+
 class Combat:
-    def __init__(self, player: Player, enemy: Enemy):
+    def __init__(self, player:Player, enemy:Enemy):
         self.player = player
         self.enemy = enemy
+        self.win_by_negotiation=False
+        self.is_lost=False
 
     def start_combat(self):
         delay_print(f'You are facing {self.enemy.name}!')
+        self.enemy.display_stats()
         while self.player.hp > 0 and self.enemy.hp > 0:
+            print("\nPlayer's status:")
+            print(colored(f'HP: {self.player.hp}', 'green'))
+            print("\nEnemy's status:")
+            print(colored(f'HP: {self.enemy.hp}', 'red'))
             print("\nWhat will you do?")
-            print("1. Attack")
-            print("2. Defend")
+            print(colored("1. Attack",'light_red'))
+            print(colored("2. Defend",'grey'))
             if self.enemy.negotiable:
-                print("3. Negotiate")
+                print(colored("3. Negotiate",'yellow'))
             action = safe_input("Choose your action (1-2 or 3): ", [1, 2] + ([3] if self.enemy.negotiable else []))
-            
+
             if action == 1:
                 self.attack()
             elif action == 2:
                 self.defend()
-            elif action == 3:
+            elif action == 3 and self.enemy.negotiable:
                 self.negotiate()
-            
+
             if self.enemy.hp > 0:
                 self.enemy_attack()
 
-        if self.player.hp <= 0:
-            delay_print("You have been defeated!")
-        elif self.enemy.hp <= 0:
-            delay_print(f"You have defeated {self.enemy.name}!")
 
     def attack(self):
-        player_damage = max(0, self.player.strength + random.randint(-2, 2) - self.enemy.armor)
+        player_damage = max(0, self.player.strength + random.randint(-2, 5))
         self.enemy.hp -= player_damage
-        delay_print(f'You attack {self.enemy.name}, dealing {player_damage} damage!')
+        delay_print(colored(f'You attack {self.enemy.name}, dealing {player_damage} damage!','green'))
 
         if self.enemy.hp <= 0:
-            delay_print(f'{self.enemy.name} has been defeated.')
+            delay_print(colored(f'{colored(self.enemy.name,attrs=['bold'])} has been defeated.','green'))
 
     def defend(self):
         block = random.randint(2, 5)
-        self.player.armor += block
-        delay_print(f'You raise your defenses, increasing armor by {block}!')
-
+        delay_print(colored(f'You raise your defenses, reducing potential damage by {block} next turn!','green'))
+        self.player.temp_defense = block 
     def negotiate(self):
         chance = random.randint(1, 10) + self.player.charisma - self.enemy.charisma
         if chance > 8:
-            delay_print(f'You successfully negotiate with {self.enemy.name}. The fight ends peacefully!')
+            delay_print(colored(f'You successfully negotiate with {colored(self.enemy.name,attrs=['bold'])}. The fight ends peacefully!','green'))
             self.enemy.hp = 0
+            self.win_by_negotiation=True
         else:
-            delay_print(f'{self.enemy.name} refuses to negotiate and continues the attack!')
+            delay_print(colored(f'{colored(self.enemy.name,attrs=['bold'])} refuses to negotiate and continues the attack!','red'))
 
     def enemy_attack(self):
-        enemy_damage = max(0, self.enemy.strength + random.randint(-2, 2) - self.player.armor)
+        enemy_damage = max(0, self.enemy.strength + random.randint(-2, 5))
+        if hasattr(self.player, 'temp_defense'):
+            enemy_damage = max(0, enemy_damage - self.player.temp_defense) 
+            del self.player.temp_defense
+
         self.player.hp -= enemy_damage
-        delay_print(f'{self.enemy.name} attacks you, dealing {enemy_damage} damage!')
-        if enemy_damage > 0:
-            self.player.armor = max(0, self.player.armor - 1)  # Armor degrades after taking damage
+        delay_print(colored(f'{self.enemy.name} attacks you, dealing {enemy_damage} damage!','red'))
 
-def combat(player: Player, enemy: Enemy):
-    delay_print(f'You are facing {enemy.name}!')
-    while player.hp > 0 and enemy.hp > 0:
-        print("\nWhat will you do?")
-        print("1. Attack")
-        print("2. Defend")
-        if enemy.negotiable:
-            print("3. Negotiate")
-        
-        action = safe_input("Choose your action (1-2 or 3): ", [1, 2] + ([3] if enemy.negotiable else []))
-
-        if action == 1:
-            player_attack(player, enemy)
-        elif action == 2:
-            defend(player, enemy)
-        elif action == 3 and enemy.negotiable:
-            negotiate(player, enemy)
-
-        if enemy.hp > 0:
-            enemy_turn(player, enemy)
-
-        print("\nPlayer's status:")
-        player.display_stats()
-        print("\nEnemy's status:")
-        print(colored(f'HP: {enemy.hp}', 'light_red'))
-
-    if player.hp <= 0:
-        delay_print("You have been defeated!")
-    elif enemy.hp <= 0:
-        delay_print(f"You have defeated {enemy.name}!")
-
-def player_attack(player: Player, enemy: Enemy):
-    damage = max(player.strength - enemy.armor, 0) + random.randint(0, 5)
-    delay_print(f"You attack {enemy.name} and deal {damage} damage!")
-    enemy.hp -= damage
-
-def defend(player: Player, enemy: Enemy):
-    defense = player.armor + random.randint(1, 5)
-    delay_print(f"You prepare to defend, increasing your defense by {defense}!")
-
-def negotiate(player: Player, enemy: Enemy):
-    success_chance = player.charisma + random.randint(0, 10) - enemy.charisma
-    if success_chance > 5:
-        delay_print(f"You successfully negotiate with {enemy.name}. The fight ends peacefully.")
-        enemy.hp = 0
-    else:
-        delay_print(f"{enemy.name} refuses to negotiate!")
-
-def enemy_turn(player: Player, enemy: Enemy):
-    damage = max(enemy.strength - player.armor, 0) + random.randint(0, 5)
-    delay_print(f"{enemy.name} attacks you and deals {damage} damage!")
-    player.hp -= damage
+        if self.player.hp <= 0:
+            delay_print(colored("YOU HAVE BEEN DEFEATED!",'red'))
+            self.is_lost=True
+        elif self.enemy.hp <= 0:
+            delay_print(colored(f'YOU HAVE DEFEATED {self.enemy.name.upper}!','green'))
 
  
 CHARACTERS_TO_CHOOSE=[
-    AvailableCharacter('Edward',8,6,5,7,100,0,'Young Knight',24,'Edward is a descendant of an ancient line of knights who has returned to his homeland after extensive training with swordmasters. He seeks glory and adventure to restore his family’s good name. He has a strong sense of justice and is ready to help those in need.','Edward excels in combat and can influence other characters with his charisma. However, his straightforwardness can sometimes lead him into difficult situations.'),
-    AvailableCharacter('Liam',6,8,7,5,100,0,'Master Weaponsmith',28,'Liam is a talented blacksmith who grew up in a village dominated by men. Since childhood, he helped his father in the forge and learned not only to create weapons but also to fight. He wants to prove that a man can be not only a blacksmith but also a great warrior.','He possesses unique skills in weapon creation and modification. His agility helps him avoid conflicts and improves his performance in battles. However, his low charisma may hinder interactions with others.'),
-    AvailableCharacter('Richard',7,9,6,8,100,0,'Bandit Leader',32,'Richard is a cunning and charismatic bandit who was once a noble knight but abandoned his honor after betrayal. He gathered a group of outlaws around him and now uses his skills for theft and manipulation.','Richard can use his charisma to manipulate other characters. His agility makes him a dangerous opponent in combat. However, his moral principles sometimes hinder him from making tough decisions.'),
-    AvailableCharacter('Gregory',3,4,9,7,100,0,'Wise Healer',45,'Gregory is a well-known healer who has devoted his life to studying herbs and healing. He possesses deep knowledge of medicine and the history of his people. His wisdom and experience make him an important ally.','He can heal the wounded and provide useful advice, which can help the player in difficult situations. However, his low physical attributes make him vulnerable in battles.'),
-    AvailableCharacter('Daniel',4,9,8,6,100,0,'Skilled Spy',30,'Daniel is a spy who has worked for various lords and kings. He grew up in poverty and learned to survive in harsh conditions, using his skills in stealth and manipulation. Now, he is looking for opportunities to improve his life and perhaps find his place in the world.','He skillfully avoids conflicts and can gather information without drawing attention. His agility allows him to infiltrate guarded places. However, he is not strong in combat, which makes him reliant on others.')
+    AvailableCharacter('Edward',8,6,5,7,100,'Young Knight',24,'Edward is a descendant of an ancient line of knights who has returned to his homeland after extensive training with swordmasters. He seeks glory and adventure to restore his family’s good name. He has a strong sense of justice and is ready to help those in need.','Edward excels in combat and can influence other characters with his charisma. However, his straightforwardness can sometimes lead him into difficult situations.'),
+    AvailableCharacter('Richard',7,9,6,8,100,'Bandit Leader',32,'Richard is a cunning and charismatic bandit who was once a noble knight but abandoned his honor after betrayal. He gathered a group of outlaws around him and now uses his skills for theft and manipulation.','Richard can use his charisma to manipulate other characters. His agility makes him a dangerous opponent in combat. However, his moral principles sometimes hinder him from making tough decisions.'),
+    AvailableCharacter('Daniel',4,9,8,6,100,'Skilled Spy',30,'Daniel is a spy who has worked for various lords and kings. He grew up in poverty and learned to survive in harsh conditions, using his skills in stealth and manipulation. Now, he is looking for opportunities to improve his life and perhaps find his place in the world.','He skillfully avoids conflicts and can gather information without drawing attention. His agility allows him to infiltrate guarded places. However, he is not strong in combat, which makes him reliant on others.')
     ]
 
 def CharacterChoose():
@@ -219,4 +162,33 @@ def CharacterChoose():
         usr = safe_input('',[1,2])
         if usr == 1:
             break
-    return Player(CHARACTERS_TO_CHOOSE[choice].name,CHARACTERS_TO_CHOOSE[choice].strength,CHARACTERS_TO_CHOOSE[choice].agility,CHARACTERS_TO_CHOOSE[choice].intelligence,CHARACTERS_TO_CHOOSE[choice].charisma,CHARACTERS_TO_CHOOSE[choice].nickname,CHARACTERS_TO_CHOOSE[choice].age,CHARACTERS_TO_CHOOSE[choice].bio,CHARACTERS_TO_CHOOSE[choice].features)
+    return Player(CHARACTERS_TO_CHOOSE[choice].name,CHARACTERS_TO_CHOOSE[choice].strength,CHARACTERS_TO_CHOOSE[choice].agility,CHARACTERS_TO_CHOOSE[choice].intelligence,CHARACTERS_TO_CHOOSE[choice].charisma)
+class Event():
+    def __init__(self,player:Player,difficulty:str,required_skill:str=None):
+        self.player=player
+        self.difficulty=difficulty
+        self.required_skill=required_skill
+        self.DIFFICULTY_DEFINITION={
+                'easy':round(random.uniform(30,60),1),
+                'medium':round(random.uniform(20,40),1),
+                'hard':round(random.uniform(5,25),1)
+            }
+        self.basic_success_chance = self.DIFFICULTY_DEFINITION[self.difficulty]
+        self.COLORS={
+            'agility':'green',
+            'strength':'red',
+            'intelligence':'cyan',
+            'charisma':'yellow'
+        }
+        self.PLAYER_SKILL={
+            'agility':self.player.agility,
+            'strength':self.player.strength,
+            'intelligence':self.player.intelligence,
+            'charisma':self.player.charisma
+        }
+    def define_succes_including_skills(self)->float:
+        success = float(self.PLAYER_SKILL[self.required_skill]*5)
+        self.basic_success_chance+=success
+        return success
+    def take_a_try(self)->bool:
+        return self.basic_success_chance>random.uniform(0,100)
--- Periode 1/Week 9/main.py
+++ Periode 1/Week 9/main.py
@@ -1,4 +1,135 @@
 from game_utils import *
+from os import system
+print('CHOOSE YOUR CHARACTER: ')
+player = CharacterChoose()
+SKILLS_COLORS={
+    'agility':'green',
+    'strength':'red',
+    'intelligence':'cyan',
+    'charisma':'yellow'
+}
+system('cls')
+system('clear')
+delay_print('LORE: ')
+delay_print('After many years of wandering, you have returned to your homeland, spurred by strange rumors circulating among the local peasants about unusual events occurring in the abandoned castle on the high hill. Many associate the disappearances of local residents with this place, while the corrupt royal guards turn a blind eye to the situation.')
+sleep(1)
+delay_print('The only hope is you.')
+sleep(1)
+#CASTLE WALL
+delay_print('Late night. There is snow all around. You climb the hill and stand at the castle walls. How would you get inside?')
+print('')
+while True:
+    event=Event(player,'easy','agility')
+    print(f'1. Climb the wall [{event.basic_success_chance}%] {colored(f'(+{event.define_succes_including_skills()}% DUE TO {event.required_skill.upper()} SKILLS)',color=event.COLORS[event.required_skill])}\n2. Go around the castle and try to find another entrance')
+    castle_wall=safe_input('',[1,2])
+    if castle_wall == 1:
+        if event.take_a_try():
+            delay_print('You easily climb the wall and go down the stairs on the other side')
+            sleep(1)
+            delay_print("You go down the stairs and suddenly a skeleton appears from around the corner. He has a sword and helmet and definitely doesn't look friendly. You have no other option but fight.")
+            break
+        else:
+            delay_print('You clumsily climb the wall, but suddenly you slip and fall')
+            delay_print(colored('-20 HP','light_red'))
+            player.hp-=20
+            player.is_dead()
+    elif castle_wall == 2:
+        delay_print('You go to the other side of castle')
+        sleep(1)
+        delay_print('You see that the other side of castle is partially destroyed and there is a big hole in the wall, but this passage is guarded by a skeleton with sword and helmet. You have no other option but fight.')
+        break
+print('')
+skeleton=Enemy('Skeleton',1,0,0,0,30,False)
+combat=Combat(player,skeleton)
+combat.start_combat()
+if combat.is_lost:quit()
 
-delay_print('CHOOSE YOUR CHARACTER: ')
-PLAYER = CharacterChoose()
+# Event: Entering the Castle
+delay_print('You enter the dark castle. The walls are cold and the air is thick with the smell of decay. Suddenly, a loud noise echoes through the halls...')
+sleep(1)
+
+# Event 1: Trap Room - Use Strength
+delay_print('You find yourself in a room with a large door that suddenly slams shut behind you. There are chains hanging from the ceiling, and the floor begins to rise slowly.')
+delay_print('The room is about to be crushed. You need to find a way to stop the mechanism before it’s too late.')
+event = Event(player, 'medium', 'strength')
+print(f'1. Use your strength to try to pull the chains and stop the trap [{event.basic_success_chance}%] {colored(f"(+{event.define_succes_including_skills()}% DUE TO {event.required_skill.upper()} SKILLS)", color=event.COLORS[event.required_skill])}\n2. Search the room for a switch or lever')
+trap_room = safe_input('', [1, 2])
+
+if trap_room == 1:
+    if event.take_a_try():
+        delay_print('You manage to break the chains with your raw strength, stopping the trap just in time.')
+        delay_print('You breathe a sigh of relief but know this is only the beginning.')
+    else:
+        delay_print('You pull on the chains with all your might, but the pressure becomes too much. The ceiling crushes you.')
+        quit(colored('You are dead!', 'light_red'))
+else:
+    delay_print('You search the room and find a lever hidden behind a tapestry. You pull it, and the room resets to its original state.')
+    delay_print('You move forward, ready for whatever awaits next.')
+    
+# Event 2: Hallway with the Royal Guards - Use Charisma
+delay_print('You move deeper into the castle, and soon find yourself face-to-face with two royal guards blocking a passage.')
+delay_print('They look at you suspiciously, their weapons drawn. You need to convince them to let you pass.')
+event = Event(player, 'medium', 'charisma')
+print(f'1. Use your charisma to try to convince the guards to let you pass [{event.basic_success_chance}%] {colored(f"(+{event.define_succes_including_skills()}% DUE TO {event.required_skill.upper()} SKILLS)", color=event.COLORS[event.required_skill])}\n2. Draw your weapon and prepare for a fight')
+
+royal_guards = safe_input('', [1, 2])
+
+if royal_guards == 1:
+    if event.take_a_try():
+        delay_print('You speak calmly and confidently, and the guards are convinced that you are on official business. They step aside and let you pass.')
+    else:
+        delay_print('You try to bluff, but the guards are not fooled. They draw their weapons and prepare to fight!')
+        combat = Combat(player, Enemy('Royal Guard', 5, 2, 2, 0, 50, False))
+        combat.start_combat()
+        if combat.is_lost:
+            delay_print('The guards overpower you, and you are thrown into the dungeon where your fate is sealed.')
+            delay_print('Game Over.')
+            quit()
+
+else:
+    delay_print('You prepare to fight the guards, drawing your weapon.')
+    combat = Combat(player, Enemy('Royal Guard', 5, 2, 2, 0, 50, False))
+    combat.start_combat()
+    if combat.is_lost:
+        delay_print('The guards overpower you, and you are thrown into the dungeon where your fate is sealed.')
+        delay_print('Game Over.')
+        quit()
+
+# Event 3: The Final Boss - Use Intelligence
+delay_print('Finally, you reach the throne room. The air is thick with dark magic. Standing before you is a powerful sorcerer who has been causing the disappearances.')
+delay_print('He turns and faces you, his eyes glowing with malice.')
+event = Event(player, 'hard', 'intelligence')
+print(f'1. Use your intelligence to try to decipher the ancient runes on the walls and weaken the sorcerer [{event.basic_success_chance}%] {colored(f"(+{event.define_succes_including_skills()}% DUE TO {event.required_skill.upper()} SKILLS)", color=event.COLORS[event.required_skill])}\n2. Rush forward and engage in combat')
+
+final_battle = safe_input('', [1, 2])
+
+if final_battle == 1:
+    if event.take_a_try():
+        delay_print('You quickly decipher the runes and cause a magical backlash that weakens the sorcerer. His power diminishes, making the fight much easier.')
+        delay_print('You strike him down and bring peace to the land.')
+        delay_print(colored('You have won!', 'green'))
+    else:
+        delay_print('You try to read the runes, but they are too complex. The sorcerer unleashes a powerful spell that overwhelms you.')
+        delay_print(colored('You are dead!', 'light_red'))
+else:
+    delay_print('You charge toward the sorcerer, sword in hand, but he casts a spell that knocks you off your feet.')
+    combat = Combat(player, Enemy('Dark Sorcerer', 8, 8, 8, 8, 100, True))
+    combat.start_combat()
+    if combat.is_lost:
+        delay_print('The dark magic overwhelms you, and the last thing you hear is the laughter of the sorcerer as your life fades away.')
+        delay_print('Game Over.')
+    elif not combat.is_lost and not combat.win_by_negotiation:
+        delay_print('With the sorcerer defeated, the curse on the land is lifted. You are hailed as a hero by the grateful villagers.')
+        delay_print('The once-abandoned castle is now a place of peace and prosperity.')
+        delay_print('Congratulations, you have saved your homeland!')
+    elif not combat.is_lost and combat.win_by_negotiation:
+        delay_print('You step forward, raising your hands in peace, your voice calm and steady.')
+        delay_print('"I see now the true power you wield," you say. "This land is broken, ruled by weak kings and fools. I could help you reshape it... if you let me."')
+        delay_print('The sorcerer pauses, eyeing you with suspicion. Then a dark smile spreads across his face.')
+        delay_print('"You have a sharp mind," he says, "and ambition. I could use someone like you."')
+        delay_print('Without hesitation, you lower your weapon and kneel before him, swearing your allegiance. The sorcerer nods approvingly.')
+        
+        delay_print('Together, you begin to weave a new order. The kingdom falls into darkness, with you as his right hand, wielding power and magic beyond imagination.')
+        delay_print('The villagers, the peasants, and the royalty who once resisted you are all crushed beneath your new reign.')
+
+        delay_print(colored('The world will know your name... as the one who joined the dark power and reshaped the fate of the land.', 'red'))
