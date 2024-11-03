5f1224482fd448e17ca7e3507711bb25b0f9fc26
b0vdurr <99080640@mydavinci.nl>
2024-11-03 01:39:34 +0100

    Update script.js

--- Periode 3/Week 30/lightswitch/script.js
+++ Periode 3/Week 30/lightswitch/script.js
@@ -1,10 +1,21 @@
+document.body.style.background='black'
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'Switch light on';
 document.body.appendChild(btn);
-
-// schijf hier tussen je code
-
-
-
-// schijf hier tussen je code
+var light_on = false;
+btn.addEventListener('click', switchLight);
+function switchLight(){
+    if (light_on ==true){
+        btn.innerHTML='Switch light on';
+        document.body.style.background='black'
+        light_on=false;
+        console.log('Light is off');
+    }
+    else{
+        btn.innerHTML='Switch light off';
+        document.body.style.background='yellow'
+        light_on=true;
+        console.log('Light is on');
+    }
+}
